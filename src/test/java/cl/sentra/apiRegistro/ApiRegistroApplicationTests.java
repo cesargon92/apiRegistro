5882e47eef32efa83d361bf33f7123812b6633af
César González <cgonzalez@sentra.cl>
2019-11-29 09:55:38 -0300

    commit inicial

---
+++ src/test/java/cl/sentra/apiRegistro/ApiRegistroApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sentra.apiRegistro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegistroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
